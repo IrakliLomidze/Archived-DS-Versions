574010c06e409b55a2346635e55ad16fced19bfb
Irakli Lomidze <Irakli@Lomidze.biz>
2024-08-29 19:34:34 +0400

    DS 1.10

---
+++ Codex DS 1.10/DS110/__Archive/SystemInstallerDS18/SystemInstallerDS18/DONNETFX.cs
@@ -0,0 +1,237 @@
+﻿using System;
+
+
+using Microsoft.Win32;
+
+namespace CodexInstaller
+{
+    class DONNETFX
+    {
+
+
+
+        // .NET Framework 4.5 +
+        private static int Get45or451FromRegistry()
+        {
+            using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\"))
+            {
+                int releaseKey = Convert.ToInt32(ndpKey.GetValue("Release"));
+
+                return releaseKey;
+            //    if (true)
+            //    {
+            //        Console.WriteLine("Version: " + CheckFor45DotVersion(releaseKey));
+            //    }
+            //
+            }
+        }
+
+        // Checking the version using >= will enable forward compatibility,  
+        // however you should always compile your code on newer versions of 
+        // the framework to ensure your app works the same. 
+   //     public static string CheckFor45DotVersion(int releaseKey)
+        public static string CheckFor45DotVersion()
+   
+        {
+            int releaseKey = Get45or451FromRegistry();
+
+            if ((releaseKey >= 379893))
+            {
+                return "OK: .NET Framework 4.5.2";
+            }
+            if ((releaseKey >= 378675))
+            {
+                return "OK: .NET Framework 4.5.1";
+            }
+            if ((releaseKey >= 378389))
+            {
+                return "OK: .NET Framework 4.5";
+            }
+            // This line should never execute. A non-null release key should mean 
+            // that 4.5 or later is installed. 
+            return "WARRNING: No .NET Framework 4.5 or later version detected";
+        }
+
+        public static string CheckFor46_48DotVersion()
+        {
+            int releaseKey = Get45or451FromRegistry();
+
+            if ((releaseKey == 393295) || (releaseKey == 393297))
+            {
+                return "OK: .NET Framework 4.6";
+            }
+
+            if ((releaseKey == 394254) || (releaseKey == 394271))
+            {
+                return "OK: .NET Framework 4.61";
+            }
+
+            if ((releaseKey == 394802) || (releaseKey == 394806))
+            {
+                return "OK: .NET Framework 4.62";
+            }
+
+            if ((releaseKey == 460798) || (releaseKey == 460798))
+            {
+                return "OK: .NET Framework 4.7";
+            }
+
+
+            if ((releaseKey == 461308) || (releaseKey == 461310))
+            {
+                return "OK: .NET Framework 4.71";
+            }
+
+            if ((releaseKey == 461808) || (releaseKey == 461814))
+            {
+                return "OK: .NET Framework 4.72";
+            }
+
+            if ((releaseKey == 528040) || (releaseKey == 528372) || (releaseKey == 528049))
+            {
+                return "OK: .NET Framework 4.8";
+            }
+
+
+            if (releaseKey > 528372) return "OK: .NET Framework 4.8 or above";
+
+
+            // This line should never execute. A non-null release key should mean 
+            // that 4.6 or later is installed. 
+            return "WARRNING: No .NET Framework 4.8 or later version detected";
+
+        }
+        public static int CheckFor45DotVersion_i()
+        {
+            int releaseKey = Get45or451FromRegistry();
+
+            if ((releaseKey >= 379893))
+            {
+                // .NET Framework 4.5.2
+                return 2;
+            }
+            if ((releaseKey >= 378675))
+            {
+                // .NET Framework 4.5.1 installed with Windows 8.1 or Windows Server 2012 R2
+                return 1;
+            }
+
+            if ((releaseKey >= 378758))
+            {
+                // .NET Framework 4.5.1 installed on Windows 8, Windows 7
+                return 1;
+            }
+
+            if ((releaseKey >= 378389))
+            {
+                // .NET Framework 4.5
+                return 0;
+            }
+            // This line should never execute. A non-null release key should mean 
+            // that 4.5 or later is installed. 
+            return -1;
+        }
+
+        ////public static int CheckFor46DotVersion_i()
+        ////{
+        ////    int releaseKey = Get45or451FromRegistry();
+
+        ////    System.OperatingSystem osInfo = System.Environment.OSVersion;
+        ////    if ((releaseKey == 393295) || (releaseKey == 393297))
+        ////    {   //.NET Framework 4.6 installed on all OS versions other than Windows 10  (393297)
+        ////        // .NET Framework 4.6 installed on Windows 10 (393295)
+        ////        return 46;
+        ////    }
+
+        ////    if ((releaseKey == 394254) || (releaseKey == 394271))
+        ////    {   // .NET Framework 4.6.1 installed on Windows 10 November Update (394254)
+        ////        // .NET Framework 4.6.1 installed on all OS versions other than Windows 10 November Update (394271)
+        ////        return 461;
+        ////    }
+
+
+        ////    if ((releaseKey == 394802) || (releaseKey == 394806))
+        ////    {   // .NET Framework 4.6.2 installed on  Windows 10 Anniversary (394802)
+        ////        // .NET Framework 4.6.2 installed on all OS versions other than Windows 10 November Update (394806)
+        ////        return 462;
+        ////    }
+
+
+
+        ////    if ((releaseKey == 460798) || (releaseKey == 460798))
+        ////    {
+        ////        return 470;
+        ////    }
+
+
+        ////    if ((releaseKey == 461308) || (releaseKey == 461310))
+        ////    {
+        ////        return 471;
+        ////    }
+
+        ////    if (releaseKey > 461310) return 490;
+        ////        //if (osInfo.Version.Major == 10)
+        ////        //{
+        ////        //    if ((releaseKey == 393295))
+        ////        //    {
+        ////        //        return 46;
+        ////        //    }
+        ////        //}
+        ////        //else
+        ////        //{
+        ////        //    if ((releaseKey == 393297))
+        ////        //    {
+        ////        //        return 46;
+        ////        //    }
+        ////        //}
+
+
+        ////        return -1;
+        ////}
+        // Codex Segment
+        public static int CheckFor48DotVersion_i()
+        {
+            int releaseKey = Get45or451FromRegistry();
+
+            System.OperatingSystem osInfo = System.Environment.OSVersion;
+            if ((releaseKey == 460798) || (releaseKey == 460805))
+            {   //.NET Framework 4.7 installed on all OS   (460805)
+                // .NET Framework 4.7 Windows 10 Creators Update (460798)
+                return 470;
+            }
+
+            if ((releaseKey == 461308) || (releaseKey == 461310))
+            {   //.NET Framework 4.7.1 installed on Windows 10 Fall Creators Update	461308
+                // .NET Framework 4.7.1 installed on all other Windows OS versions	461310
+                return 471;
+            }
+
+
+            if ((releaseKey == 461808) || (releaseKey == 461814))
+            {
+                //On Windows 10 April 2018 Update and Windows Server, version 1803: 461808
+                //On all Windows operating systems other than Windows 10 April 2018 Update and Windows Server, version 1803: 461814
+                return 472;
+            }
+
+            if ((releaseKey == 528040) || (releaseKey == 528372) || (releaseKey == 528049))
+            {
+                ////On Windows 10 May 2019 Update and Windows 10 November 2019 Update: 528040
+                ////On Windows 10 May 2020 Update and Windows 10 October 2020 Update: 528372
+                ////On all other Windows operating systems(including other Windows 10 operating systems): 528049
+                return 480;
+            }
+
+
+            if (releaseKey > 528372) { 
+                return 481;
+            }
+
+
+
+            return -1;
+        }
+   
+
+    }
+}
---
+++ Codex DS 1.10/DS110/__Archive/SystemInstallerDS18/SystemInstallerDS18/Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Windows.Forms;
+using CodexInstaller;
+using System.Diagnostics;
+
+namespace ILG.Codex.CodexR4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static SplashScreen sp;
+
+        public static bool   Copying;
+        
+
+   
+   
+        public delegate void ShowProgressDelegate(int progress, string Str);
+
+    
+       
+
+        delegate void ProcessDelegate();
+
+        String CurrentDirctory;
+
+        int pagenumber;
+        int way = -1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SuspendLayout();
+            pagenumber = 1;
+
+            CurrentDirctory = System.Environment.CurrentDirectory;
+
+
+            if (Properties.Settings.Default.InstallConfiguration == 2)
+            {
+
+                Check_Label_Codex.Text = "Codex DS 1.8 Client";
+
+            }
+
+
+
+
+
+            Identify_Current_State();
+
+            ResumeLayout();
+            this.Installing_Activity_Indicator.Stop();
+
+            if (Form1.sp.Visible == true) Form1.sp.Hide();
+       
+        }
+
+     
+
+   
+
+
+        private void ultraButton1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Quite from Application ?", "", System.Windows.Forms.MessageBoxButtons.YesNo, System.Windows.Forms.MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes) Close();
+        }
+
+
+        private void ultraButton2_Click(object sender, EventArgs e)
+        {
+            if (CurrentNavigationPosition == 1)
+            {
+                InstallerTab.SelectedTab = InstallerTab.Tabs[0];
+                InstallerTab.ActiveTab = InstallerTab.Tabs[0];
+            }
+        }
+
+
+
+
+
+        
+        bool NETFX48_isRequred = false;
+        bool AcrobatDC_isRequred = false;
+
+
+        void Identify_Current_State()
+        {
+
+            #region .NET 4.8
+            if (DONNETFX.CheckFor48DotVersion_i() >= 480)
+            {
+                Check_Label_NetFx.Checked = false;
+                NETFX48_isRequred = false;
+
+            }
+            else
+            {
+                Check_Label_NetFx.Checked = true;
+                NETFX48_isRequred = true;
+            }
+            #endregion .NET 4.8
+
+
+            CodexDSSystem codexsys = new CodexDSSystem();
+
+            String CheckKey32 = CodexDSSystem.CodexDSClientKey32; 
+            String CheckKey64 = CodexDSSystem.CodexDSClientKey64;
+            
+
+            if (codexsys.isCodexNeedToBeInstalled(CheckKey32, CheckKey64) == true)
+            {
+                Check_Label_Codex.Checked = true;
+                this.Pic_Codex.Image = ILG.Codex.CodexR4.Properties.Resources.BlueDot;
+            }
+            else
+            {
+                Check_Label_Codex.Checked = false;
+                Pic_Codex.Image = null;
+            }
+        
+
+
+             
+            this.Status_String.Text = " ";
+            UpdateDots();
+        }
+
+        int CurrentNavigationPosition = 0;
+        private void Button_Next_Click(object sender, EventArgs e)
+        {
+            if (CurrentNavigationPosition == 0)
+            {
+                Identify_Current_State();
+                InstallerTab.SelectedTab = InstallerTab.Tabs[1];
+                InstallerTab.ActiveTab = InstallerTab.Tabs[1];
+            }
+        }
+
+        private void Installation_Complte()
+        {
+            InstallerTab.SelectedTab = InstallerTab.Tabs[2];
+            InstallerTab.ActiveTab = InstallerTab.Tabs[2];
+            Button_Next.Enabled = false;
+            Button_Back.Enabled = false;
+            Button_Close.Enabled = true;
+        }
+
+        private void Link_Config_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            About f = new About();
+            f.ShowDialog();
+        }
+
+        
+
+  
+
+        private void Linlk_NetFX_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Get and Show ActualStatus for .NET
+            String S = "";
+            S = S + System.Environment.NewLine + DONNETFX.CheckFor46_48DotVersion();
+            MessageBox.Show(S);
+        }
+
+
+      
+        private void ClearDots()
+        {
+            Pic_NetFx.Image = null;
+            Pic_Codex.Image = null;
+            return;
+        }
+
+        private void UpdateDots()
+        {
+            ClearDots();
+            if (Check_Label_NetFx.Checked == true) Pic_NetFx.Image = Properties.Resources.BlueDot;
+
+            if (Check_Label_Codex.Checked == true) Pic_Codex.Image = Properties.Resources.BlueDot;
+     
+            return;
+        }
+
+        private void ultraButton1_Click_1(object sender, EventArgs e)
+        {
+
+
+            ///            if (InstallWarning == true) { MessageBox.Show("WARRNING !!! : PLEASE SEE INSTALLATION WARNINGS !!!"); return; }
+
+            UpdateDots();
+
+
+
+
+            if (Check_Label_NetFx.Checked == true)
+            {
+                bool Result = false;
+                Result = Install_NET48Process();
+                if (Result == false) return;
+            }
+
+
+            if (Check_Label_Codex.Checked == true)
+
+            {
+                bool Result = Install_CodexClient();
+                if (Result == false) return;
+                else
+                {
+                    Installation_Complte();
+                    return;
+                }
+            }
+
+
+        }
+
+        bool is_Restart_Requred = false;
+
+        private bool Install_NET48Process()
+        {
+            
+            if (NETFX48_isRequred == true)
+            {
+                String FileWithLocation = CurrentDirctory + @"\"+@"..\Packages\netframework\" + "ndp48-x86-x64-allos-enu.exe";
+                //String CommandLineParameters = @" /passive /promptrestart /showrmui /log %temp%\SP46.htm";
+                String CommandLineParameters = "";// @" /passive /norestart /showrmui /log %temp%\SP46.htm";
+
+                this.Status_String.Text = "Installing .NET Framework 4.8 ... ";
+                this.Pic_NetFx.Image = Properties.Resources.arrow_right;
+
+                // Update UI
+
+                this.Installing_Activity_Indicator.Start(true);
+
+                Process myprocess = new Process();
+                //myprocess.StartInfo.FileName = FileWithLocation;
+                //myprocess.StartInfo.Arguments = CommandLineParameters;
+
+
+          
+                Process myProcess = null;
+                myProcess = Process.Start(FileWithLocation, CommandLineParameters);
+
+                do
+                {
+                    // Some Animagioin
+                } while (!myProcess.WaitForExit(3000));
+
+                this.Installing_Activity_Indicator.Stop();
+                
+                if (myProcess.ExitCode == 1602)
+                {
+                    MessageBox.Show("Installation Caneled By you");
+                    this.Status_String.Text = "... ";
+                    this.Pic_NetFx.Image = Properties.Resources.BlueDot;
+                    return false;
+
+                    // DO Some UI Update
+                }
+
+
+                if (myProcess.ExitCode == 1603)
+                {
+                    MessageBox.Show("Error During Installation See Log Below"+ System.Environment.NewLine+ @"%temp%\SP46.htm");
+                    Process.Start(System.Environment.GetEnvironmentVariable("%temp%").ToString() + "\\SP46.htm");
+                    // DO Some UI Update
+                    this.Status_String.Text = " Error During Installation ";
+                    this.Pic_NetFx.Image = Properties.Resources.RedDot;
+                    return false;
+
+                }
+
+                if ((myProcess.ExitCode == 1641) || (myProcess.ExitCode == 3010))
+                {
+                    MessageBox.Show("Please Restart your System to Finish Installation");
+                    this.Status_String.Text = " Please Restart your System to Finish Installation ";
+                    this.Pic_NetFx.Image = Properties.Resources.BlueDot;
+                    // DO Some UI Update
+                    is_Restart_Requred = true;
+                    return false;
+                }
+
+
+
+                if ((myProcess.ExitCode == 0) )
+                {
+                    this.Status_String.Text = " ... ";
+                    this.Pic_NetFx.Image = Properties.Resources.GreenDot;
+                    return true;
+                }
+
+            }
+
+            return true;
+        }
+
+
+
+        private bool Install_CodexClient()
+        {
+
+            CodexDSSystem codexsys = new CodexDSSystem();
+
+            if (codexsys.isCodexNeedToBeInstalled(CodexDSSystem.CodexDSClientKey32, CodexDSSystem.CodexDSClientKey64) == true)
+            {
+
+                String FileWithLocation = "msiexec";
+
+
+
+                String CommandLineParameters = @"/i CodexDS19Client.msi ";/// qn IACCEPTSQLLOCALDBLICENSETERMS = YES ";
+                String WorkingDirecotry = CurrentDirctory + @"\" + @"..\CodexPackages\";
+
+                this.Status_String.Text = "Installing Codex DS 1.9 Client ... ";
+                this.Pic_Codex.Image = Properties.Resources.arrow_right;
+
+
+                Process myProcess = new Process();
+
+                myProcess.StartInfo.FileName = FileWithLocation;//  FileWithLocation;
+                myProcess.StartInfo.Arguments = "  " + CommandLineParameters;
+                myProcess.StartInfo.WorkingDirectory = WorkingDirecotry;
+                myProcess.StartInfo.UseShellExecute = false;
+
+
+                // Update UI
+                try
+                {
+                    myProcess.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+                do
+                {
+                    // Some Animagioin
+                } while (!myProcess.WaitForExit(3000));
+
+                this.Installing_Activity_Indicator.Stop();
+
+
+
+                if (myProcess.ExitCode != 0)
+                {
+                    MessageBox.Show("Error During Installation");
+                    this.Status_String.Text = " Error During Installation ";
+                    this.Pic_Codex.Image = Properties.Resources.RedDot;
+                    myProcess.Close();
+                    return false;
+                }
+
+
+
+                if ((myProcess.ExitCode == 0))
+                {
+                    this.Status_String.Text = " ... ";
+                    this.Pic_Codex.Image = Properties.Resources.GreenDot;
+                    myProcess.Close();
+                }
+
+
+
+            }
+
+            return true;
+        }
+
+   
+
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            About f = new About();f.ShowDialog();
+        }
+
+    }
+}
